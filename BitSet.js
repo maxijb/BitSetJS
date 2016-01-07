4e2c90eba76d409fefa35e249031ae06cf60a3fe
Maximiliano Benedetto <infoprisma1@gmail.com>
2016-01-07 14:23:08 +0100

    Definig BitSet and tests

---
+++ BitSet.js
@@ -0,0 +1,84 @@
+let BitSet = (function() {
+    
+    //symbol to ofuscate the data array
+    const dataSymbol = Symbol('data');
+
+    //number of bits per integer
+    const BITS = 32;
+
+    /* Private method to convert N to byte, bit */
+    const convert = function(n) {
+        if (n >= this.length) return null;
+        let byte = n / BITS | 0,
+            bit = n % BITS;
+
+        return {byte , bit};
+    }
+
+    /* Constructor 
+    @param legnth [optional] number of bits in array
+    @param value [optional] 1 or 0, initial value for all bits in the array
+    */
+    function BitSet(length=32, value=0) {
+        //cache length
+        this.length = length;
+        
+        //how many ints we need for the bits required
+        let ints = Math.ceil(length / BITS);
+
+        //initialize the backing array with the desired values
+        this[dataSymbol] = Array.apply(null, Array(ints)).map((x) => !value ? 0 : -1);
+    }
+
+    
+	/* ------------------  Public Methods --------------------*/
+
+    //get the n desired byte (calls convert to get the exact location)
+    BitSet.prototype.get = function(n) {
+        let pos = convert.call(this, n);
+        return pos ? (this[dataSymbol][pos.byte] & 1 << pos.bit) > 0 : null;
+    };
+
+    //set the n desired byte to 1 (calls convert to get the exact location)
+    BitSet.prototype.set = function(n) {
+        let pos = convert.call(this, n);
+        if (pos) this[dataSymbol][pos.byte] |= 1 << pos.bit;
+        return pos != null;
+    }
+
+    //set the n desired byte to 0 (calls convert to get the exact location)
+    BitSet.prototype.clear = function(n) {
+    	let pos = convert.call(this, n);
+		if (pos) this[dataSymbol][pos.byte] &= ~(1 << pos.bit);
+		return pos != null;
+    }
+
+    //return the backing data as an array of integers
+    BitSet.prototype.getData = function() {
+    	return this[dataSymbol];
+    }
+
+    //string type of the object
+    BitSet.prototype.toString = function() {
+        return "[object BitSet]";
+    }
+
+    //stringifies the array of bits as 00101100...
+    BitSet.prototype.stringify = function() {
+    	let buffer = "";
+        for (let i = 0; i < this.length; i++) {
+            buffer += this.get(i) ? "1" : "0";
+        }
+    	return buffer;
+    }
+
+    return BitSet;
+}());
+
+
+/* Exports for browser's global object or exports as commonJS */
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = BitSet;
+else
+    window.BitSet = BitSet;
+
---
+++ spec/bitset.test.js
@@ -0,0 +1,81 @@
+jest.autoMockOff();
+let BitSet = require('../BitSet');
+
+describe('BitSet of 1 integer', () => {
+
+	//crating a default bitset and getting its backing data 
+    let bitset = new BitSet();
+    let data = bitset.getData();
+    
+    it('Bit array length', () => expect(bitset.length).toBe(32) );
+
+    it('Type of data', () => expect(Array.isArray(data)).toBeTruthy() );
+
+    it('Length of array data', () => expect(data.length).toBe(1) );
+
+    it('First bit value', () => expect(bitset.get(0)).toBe(false) );
+
+    it('setting second bit', () => expect(bitset.set(1)).toBe(true) );
+
+    it('second bit value after setting', () => expect(bitset.get(1)).toBe(true) );
+
+    it('clearing second bit', () => expect(bitset.clear(1)).toBe(true) );
+
+    it('second bit value after clearing', () => expect(bitset.get(1)).toBe(false) );
+    
+    it('31 bit should be the last', () => expect(bitset.get(31)).toBe(false) );
+
+    it('32 bit should be out of index', () => expect(bitset.get(32)).toBe(null) );
+
+    it('stringify should return 32 0s in a row', () => expect(bitset.stringify()).toBe("00000000000000000000000000000000"));
+
+});
+
+
+
+describe('BitSet of 3 integers and all 1s', () => {
+
+	//crating biset and getting its backing data 
+    let bitset = new BitSet(96, 1);
+    let data = bitset.getData();
+    
+    it('Bit array length', () => expect(bitset.length).toBe(96) );
+
+    it('Type of data', () => expect(Array.isArray(data)).toBeTruthy() );
+
+    it('Length of array data', () => expect(data.length).toBe(3) );
+
+    it('Middle bit value', () => expect(bitset.get(48)).toBe(true) );
+
+    it('clearing middle bit', () => expect(bitset.clear(48)).toBe(true) );
+    
+    it('Middle bit value after clearing', () => expect(bitset.get(48)).toBe(false) );
+
+    it('Setting middle bit', () => expect(bitset.set(48)).toBe(true) );
+
+    it('Middle bit value after setting', () => expect(bitset.get(1)).toBe(true) );
+
+});
+
+
+describe('Mixed BitSet of 5 bits', () => {
+
+	//crating biset and getting its backing data 
+    let bitset = new BitSet(5, 1);
+    let data = bitset.getData();
+    bitset.clear(2);
+    bitset.clear(4);
+    
+    it('Bit array length', () => expect(bitset.length).toBe(5) );
+
+    it('Type of data', () => expect(Array.isArray(data)).toBeTruthy() );
+
+    it('Length of array data', () => expect(data.length).toBe(1) );
+
+    it('toString object bitset', () => expect(bitset.toString()).toBe("[object BitSet]"));
+    
+    it('stringify 5 bits', () => expect(bitset.stringify()).toBe("11010"));
+
+    it('7th bit should be out of index', () => expect(bitset.get(7)).toBe(null) );
+
+});
